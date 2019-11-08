5e0f1b99a7c23fafda09e530c64f9c40bb75e2bd
gfarrisi <gabriellafarrisi@gmail.com>
2019-11-08 10:45:57 -0500

    redesign of Admin dashboard to use modal instead of new page

--- AdminDashboard.aspx.designer.cs
+++ AdminDashboard.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace Empty_Project_Template {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnNewRequest;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
