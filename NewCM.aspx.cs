d33073858af89c4689a7378cfce8368cdfa24b8d
gfarrisi <gabriellafarrisi@gmail.com>
2019-11-17 15:55:30 -0500

    added screenshot and submisison stuff

--- NewCM.aspx.cs
+++ NewCM.aspx.cs
@@ -69,9 +69,67 @@ namespace Empty_Project_Template
 
 
                 }
-             
+                buildSubmission();
+
+
             }
             
+        }
+        public void buildSubmission()
+        {
+            int RequestID = 9999;
+            RequestTypes types = new RequestTypes();
+            foreach (Request requestType in types.requestTypes)
+            {
+                if (requestType.RequestID == RequestID)
+                {
+                    foreach (Question question in requestType.requestQuestions)
+                    {
+                        string question_text = question.Question_Text;
+                        string question_control = question.Question_Control;
+                        string question_order = question.Question_Order;
+                        List<string> question_options = question.Question_Options;
+
+                        Label lblText = new Label();
+                        lblText.Text = question_text;
+                        lblText.CssClass = "form-text";
+                        panelCM.Controls.Add(lblText);
+
+                        if (question_control == "RadioButtonList")
+                        {
+                            foreach (string option in question_options)
+                            {
+                                RadioButton rbOption = new RadioButton();
+                                rbOption.Text = option;
+                                rbOption.CssClass = "form-check";
+                                panelCM.Controls.Add(rbOption);
+                            }
+                        }
+                        else if (question_control == "TextBox")
+                        {
+                            TextBox txtAnswer = new TextBox();
+                            txtAnswer.CssClass = "form-control";
+                            panelCM.Controls.Add(txtAnswer);
+                        }
+                        else if (question_control == "DropDownList")
+                        {
+                            DropDownList ddlOptions = new DropDownList();
+                            ddlOptions.CssClass = "dropdown";
+
+                            foreach (string option in question_options)
+                            {
+                                ddlOptions.Items.Add(option);
+                                panelCM.Controls.Add(ddlOptions);
+                            }
+
+                        }
+
+                    }
+                }
+
+
+
+            }
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
