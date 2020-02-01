38d93f6c844ca35cb7daef0aa460e7fa03663cf5
gfarrisi <gabriellafarrisi@gmail.com>
2020-02-01 18:14:06 -0500

    got request data from db and set up resonse classes

---
+++ RequestLibrary/CMRequest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Empty_Project_Template.RequestLibrary
+{
+    public class CMRequest
+    {
+        private int cmID;
+        private string cmStatus;
+        private byte[] attachment1;
+        private byte[] attachment2;
+        private byte[] attachment3;
+        private byte[] attachment4;
+        private byte[] attachment5;
+        private string questionsComments;
+        private string lastUpdateUser;
+        private DateTime lastUpdateDate;
+        private string userID;
+        private string adminID;
+        private int requestTypeID;
+        private DateTime desiredDate;
+        private List<QuestionResponse> questionResponses;
+
+        public CMRequest(string status, byte[] att1, byte[] att2, byte[] att3, byte[] att4, byte[] att5, string questComm, string lastUser,
+            DateTime lastDate, string user, string admin, int typeID, DateTime desireddate, List<QuestionResponse> responses)
+        {
+            cmStatus = status;
+            attachment1 = att1;
+            attachment2 = att2;
+            attachment3 = att3;
+            attachment4 = att4;
+            attachment5 = att5;
+            questionsComments = questComm;
+            lastUpdateUser = lastUser;
+            lastUpdateDate = lastDate;
+            userID = user;
+            adminID = admin;
+            requestTypeID = typeID;
+            desiredDate = desireddate;
+            questionResponses = responses;
+        }
+
+        public int CMID
+        {
+            get { return cmID; }
+            set { cmID = value; }
+        }
+
+        public string CMStatus
+        {
+            get { return cmStatus; }
+            set { cmStatus = value; }
+        }
+
+        public string LastUpdateUser
+        {
+            get { return lastUpdateUser; }
+            set { lastUpdateUser = value; }
+        }
+
+        public DateTime LastUpdateDate
+        {
+            get { return lastUpdateDate; }
+            set { lastUpdateDate = value; }
+        }
+
+        public string UserID
+        {
+            get { return userID; }
+            set { userID = value; }
+        }
+
+        public string AdminID
+        {
+            get { return adminID; }
+            set { adminID = value; }
+        }
+
+        public int RequestTypeID
+        {
+            get { return requestTypeID; }
+            set { requestTypeID = value; }
+        }
+    }
+}
--- RequestLibrary/Question.cs
+++ RequestLibrary/Question.cs
@@ -8,15 +8,15 @@ namespace Empty_Project_Template.RequestLibrary
     public class Question
     {
         private string question_text { get; set; }
-        private string question_order { get; set; }
         private string question_control { get; set; }
+        private int question_id { get; set; }
         private List<string> question_options { get; set; }
 
-        public Question(string text, string order, string control, List<string> options)
+        public Question(string text, string control, int id, List<string> options)
         {
             question_text = text;
             question_control = control;
-            question_control = control;
+            question_id = id;
             question_options = options;
         }
 
@@ -26,10 +26,10 @@ namespace Empty_Project_Template.RequestLibrary
             set { question_text = value; }
         }
 
-        public string Question_Order
+        public int Question_ID
         {
-            get { return question_order; }
-            set { question_order = value; }
+            get { return question_id; }
+            set { question_id = value; }
         }
         public string Question_Control
         {
