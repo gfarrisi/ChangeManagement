d52b0ff928fffe8117ee395c4e72107b063cb7d6
Samuel Mikulski <tug41822@temple.edu>
2020-04-17 17:47:35 -0400

    logout is now 100% working - does not require a browser close

--- Secure/Logout.aspx.cs
+++ Secure/Logout.aspx.cs
@@ -11,8 +11,23 @@ namespace ChangeManagementSystem.Secure
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Session.Clear();
-            Session.Abandon();
+            string url = HttpContext.Current.Request.Url.AbsoluteUri;
+            if (url.Contains("pre-stem"))
+            {
+                Session.Clear();
+                Session.Abandon();
+                Response.Cookies.Clear();
+                Response.Redirect("https://pre-stem.temple.edu/Shibboleth.sso/Logout?return=https://np-fim.temple.edu/idp/profile/Logout");
+               
+            }
+            else if (url.Contains("np-stem"))
+            {
+                Session.Clear();
+                Session.Abandon();
+                Response.Cookies.Clear();
+                Response.Redirect("https://np-stem.temple.edu/Shibboleth.sso/Logout?return=https://np-fim.temple.edu/idp/profile/Logout");
+            }
+          
         }
     }
 }
